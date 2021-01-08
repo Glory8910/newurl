7d379008c1b618ef1c4599876777fc2ab8009621
seeli <seeliwrites@gmail.com>
2021-01-08 20:58:25 +0530

    finalcommit after static

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let urlExists = require('url-exists')
 
 
 const app = express()
+app.use(express.static('public'));
 app.set("view engine", "ejs")
 const port = 3000;
 app.use(bodyparser.urlencoded({ extended: true }))
